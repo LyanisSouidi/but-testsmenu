b073369734e502032b09728123a10179cd7691d3
Lyanis Souidi <code@lyanis.sh>
2023-10-27 09:15:50 +0200

    Initialisation du projet
    Co-authored-by: Hugo Dimitrijevic <hugo.dimitrijevic@etu.u-pec.fr>
    Co-authored-by: Aissame Bai <aissame.bai@etu.u-pec.fr>

---
+++ src/fr/iutfbleau/but2/sae312023/Programme1.java
@@ -0,0 +1,7 @@
+package fr.iutfbleau.but2.sae312023;
+
+class Programme1 {
+    public static void main(String[] args) {
+        System.out.println("Outil de test");
+    }
+}
---
+++ src/fr/iutfbleau/but2/sae312023/Programme2.java
@@ -0,0 +1,7 @@
+package fr.iutfbleau.but2.sae312023;
+
+public class Programme2 {
+    public static void main(String[] args) {
+        System.out.println("Synthèse des tests");
+    }
+}
